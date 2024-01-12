96328796cbdbbe5391584a5356d1149534067344
Antonio <toninhofbn@hotmail.com>
2024-01-12 14:47:56 -0300

    desafio finalisado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criando um objeto Nokia
+        Nokia nokia = new Nokia("123456789", "Nokia", "3310");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        // Criando um objeto Iphone
+        Iphone iphone = new Iphone("987654321", "Apple", "X");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
